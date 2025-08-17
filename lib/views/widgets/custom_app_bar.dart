f1c96770a14307f6185d4075cd1b2ab696a7aa04
Nooraldeen Al_Awsaji <ngalawsaji@gmail.com>
2025-08-17 22:35:25 +0300

    create appbar by widjet custom app bar and customicon widjet

---
+++ lib/views/widgets/custom_app_bar.dart
@@ -0,0 +1,18 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+import 'package:my_note_app/views/widgets/custom_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Text('Notes', style: TextStyle(fontSize: 28)),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
