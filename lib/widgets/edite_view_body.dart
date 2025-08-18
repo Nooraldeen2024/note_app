ebe6884c70d5afb3161e4be70b5dfc06f59887d6
Nooraldeen Al_Awsaji <ngalawsaji@gmail.com>
2025-08-18 16:19:13 +0300

    adit note view ui done

--- lib/widgets/edite_view_body.dart
+++ lib/widgets/edite_view_body.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:my_note_app/widgets/Custom_app_bar.dart';
+import 'package:my_note_app/widgets/custom_text_field.dart';
 
 class EditeNoteViewBody extends StatelessWidget {
   const EditeNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        children: [
-          SizedBox(height: 50),
-          CustomAppBar(title: 'Edite Notes', icon: Icons.check),
-        ],
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            CustomAppBar(title: 'Edite Notes', icon: Icons.check),
+            SizedBox(height: 16),
+            CustomTextField(hint: 'title'),
+            SizedBox(height: 16),
+            CustomTextField(hint: 'Content', maxLines: 5),
+          ],
+        ),
       ),
     );
   }
