fbbeccf174ed6bee246ba5a245758727a4f1decd
Nooraldeen Al_Awsaji <ngalawsaji@gmail.com>
2025-08-19 19:26:41 +0300

    create note modle

---
+++ lib/models/note_models.dart
@@ -0,0 +1,13 @@
+class NoteModels {
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+
+  NoteModels({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
+}
