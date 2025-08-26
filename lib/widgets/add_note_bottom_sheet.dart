6f8bea1eafe3bb56afd8d35fc5ab537013647573
Nooraldeen Al_Awsaji <ngalawsaji@gmail.com>
2025-08-26 19:35:46 +0300

    change sheet sized

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -23,7 +23,11 @@ class AddNoteBottumsheet extends StatelessWidget {
           return AbsorbPointer(
             absorbing: state is AddNoteLoading ? true : false,
             child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16),
+              padding: EdgeInsets.only(
+                right: 16,
+                left: 16,
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
               child: const SingleChildScrollView(child: AddNoteForm()),
             ),
           );
